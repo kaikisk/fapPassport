cbb10f02e3addd5abb62a8e828142cc70382e390
kaikisk <kueobei@gmail.com>
2019-10-04 05:53:15 +0900

    commit

--- javaScript/openIndexedDB.js
+++ javaScript/openIndexedDB.js
@@ -1,25 +1,25 @@
-var db;
-var indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB;
+var idb;
+var indexeddb = window.indexeddb || window.mozIndexeddb || window.msIndexeddb;
 
-if (indexedDB) {
+if (indexeddb) {
     // データベースを削除したい場合はコメントを外します。
-    //indexedDB.deleteDatabase("mydb");
-    var openRequest = indexedDB.open("fapPassport", 1.0);
+    //indexedidb.deleteDatabase("myidb");
+    var openRequest = indexeddb.open("fapPassport", 1.0);
 
     openRequest.onupgradeneeded = function (event) {
         // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
         console.dir(event);
-        db = event.target.result;
-        var store = db.createObjectStore("fapPass", { keyPath: "id" });
+        idb = event.target.result;
+        var store = idb.createObjectStore("fapPass", { keyPath: "id" });
         store.createIndex("myvalueIndex", "myvalue");
         console.log("pass onupgradeneeded");
     }
 
 
     openRequest.onsuccess = function (event) {
-        db = event.target.result;
+        idb = event.target.result;
         console.log("pass onsuccess");
-        console.dir(db);
+        console.dir(idb);
     }
 } else {
     window.alert("このブラウザではIndexed DataBase API は使えません。");
