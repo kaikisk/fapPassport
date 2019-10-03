8f45e286c0ec4d9ae5656f9986babb7373b1a595
kaikisk <kueobei@gmail.com>
2019-10-04 05:39:06 +0900

    commit

--- javaScript/openIndexedDB.js
+++ javaScript/openIndexedDB.js
@@ -2,29 +2,27 @@ var db;
 var indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB;
 
 if (indexedDB) {
-db.close();
-// データベースを削除したい場合はコメントを外します。
-//indexedDB.deleteDatabase("mydb");
-var openRequest = indexedDB.open("fapPassport", 1.0);
-    
-openRequest.onupgradeneeded = function(event) {
-    // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
-    console.dir(event);
-    db = event.target.result;
-    var store = db.createObjectStore("store1", { keyPath: "id"});
-    store.createIndex("myvalueIndex", "myvalue");
-    console.log("pass onupgradeneeded");
-}
+    // データベースを削除したい場合はコメントを外します。
+    //indexedDB.deleteDatabase("mydb");
+    var openRequest = indexedDB.open("fapPassport", 1.0);
 
-    
-openRequest.onsuccess = function(event) {
+    openRequest.onupgradeneeded = function (event) {
+        // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
+        console.dir(event);
         db = event.target.result;
-        console.log("success");
+        var store = db.createObjectStore("fapPass", { keyPath: "id" });
+        store.createIndex("myvalueIndex", "myvalue");
+        console.log("pass onupgradeneeded");
+    }
+
+
+    openRequest.onsuccess = function (event) {
+        db = event.target.result;
+        console.log("pass onsuccess");
         console.dir(db);
-        db.close();
     }
 } else {
-window.alert("このブラウザではIndexed DataBase API は使えません。");
+    window.alert("このブラウザではIndexed DataBase API は使えません。");
 }
 
 
