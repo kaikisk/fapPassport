263d12d2a093d437adef55b55e0103e7973ab7b4
kaikisk <kueobei@gmail.com>
2019-10-18 02:16:32 +0900

    commit

--- javaScript/login.js
+++ javaScript/login.js
@@ -58,7 +58,7 @@ function getUserData(key) {
             console.log("p2");
             requestName.onsuccess = function (event) {
                 console.log("key: " + key + ", value: " + event.target.result.myvalue);
-                return Promise.resolve(event.target.result.myvalue);
+                resolve(event.target.result.myvalue);
             }
             db.close();
         }
@@ -81,20 +81,6 @@ $(function () {
     } else {
         document.getElementById("signup").style.display = "none";
     }
-
-    const resolvedProm = Promise.resolve(33);
-
-    let thenProm = resolvedProm.then((value) => {
-        console.log("this gets called after the end of the main stack. the value received and returned is: " + value);
-        return value;
-    });
-    // instantly logging the value of thenProm
-    console.log(thenProm);
-
-    // using setTimeout we can postpone the execution of a function to the moment the stack is empty
-    setTimeout(() => {
-        console.log(thenProm);
-    });ß
 })
 
 function clickLoginButton() {
