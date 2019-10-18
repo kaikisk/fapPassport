2e0684b9757091faff49c6fcc555161cfd038195
kaikisk <kueobei@gmail.com>
2019-10-18 12:36:57 +0900

    commit

--- javaScript/login.js
+++ javaScript/login.js
@@ -14,12 +14,47 @@
 //     }
 
 // }
+
+function createDatabase() {
+    return new Promise(function(resolve, reject) {
+        var db;
+        var indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB;
+
+        if(indexedDB) {
+            // データベースを削除したい場合はコメントを外します。
+            //indexedDB.deleteDatabase("mydb");
+            var openRequest = indexedDB.open("fapPassport");
+
+            openRequest.onupgradeneeded = function (event) {
+                // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
+                console.dir(event);
+                db = event.target.result;
+                var store = db.createObjectStore("fapPass", { keyPath: "id" });
+                store.createIndex("myvalueIndex", "myvalue");
+                console.log("pass onupgradeneeded");
+                var store1 = db.createObjectStore("photo", { keyPath: "id" });
+                store1.createIndex("myvalueIndex", "myvalue");
+            }
+
+
+            openRequest.onsuccess = function (event) {
+                db = event.target.result;
+                console.log("pass onsuccess");
+                console.dir("db: " + db);
+            }
+        } else {
+            window.alert("このブラウザではIndexed DataBase API は使えません。");
+        }
+    })
+}
+
 function getUserData(key) {
     return new Promise(function (resolve, reject) {
         var db;
         var request = indexedDB.open('fapPassport');
         request.onsuccess = function (event) {
             db = event.target.result;
+            console.dir(db);
             var ts = db.transaction(["fapPass"], "readwrite");
             var store = ts.objectStore("fapPass");
             var requestName = store.get(key);
@@ -42,41 +77,14 @@ function getUserData(key) {
 
 
 $(function () {
-    var db;
-    var indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB;
-
-    if (indexedDB) {
-        // データベースを削除したい場合はコメントを外します。
-        //indexedDB.deleteDatabase("mydb");
-        var openRequest = indexedDB.open("fapPassport");
-
-        openRequest.onupgradeneeded = function (event) {
-            // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
-            console.dir(event);
-            db = event.target.result;
-            var store = db.createObjectStore("fapPass", { keyPath: "id" });
-            store.createIndex("myvalueIndex", "myvalue");
-            console.log("pass onupgradeneeded");
-            var store1 = db.createObjectStore("photo", { keyPath: "id" });
-            store1.createIndex("myvalueIndex", "myvalue");
-        }
-
-
-        openRequest.onsuccess = function (event) {
-            db = event.target.result;
-            console.log("pass onsuccess");
-            console.dir("db: " + db);
-        }
-    } else {
-        window.alert("このブラウザではIndexed DataBase API は使えません。");
-    }
-
+    createDatabase();
     txtName = getUserData("txtName");
     txtPass = getUserData("txtPass");
     ErrText = getUserData("txtAAA");
     console.log("txtName1: " + txtName);
     console.log("txtPass1: " + txtPass);
     console.log("ErrText: " + ErrText);
+
     if (txtName == null && txtPass == null) {
         document.getElementById("signin").style.display = "none";
         console.log("")
