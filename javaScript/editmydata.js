44a346376a43b3eefb57f3b54193ffb225e871b5
kaikisk <kueobei@gmail.com>
2019-10-23 23:14:05 +0900

    commit

--- javaScript/editmydata.js
+++ javaScript/editmydata.js
@@ -2,11 +2,13 @@ $(function() {
     var keys = ['telFam', 'telAtten','bloodType','height','weight',
     'medi1','medi2','medi3',
     'anam','anam2','anam3',
-    'txtName','txtMail','txtPass'];
+    'txtName','txtPass'];
     var saveData = new Array();
+    // var loadData = new Array();
     for(var i = 0; i < keys.length; i++) {
         load(keys[i]);
     }
+
     $('#update').click(function(e){
         for(var i = 0; i < keys.length; i++) {
             saveData.push(save(keys[i]));
