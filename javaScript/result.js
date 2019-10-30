3ed39b64c00b97c253130878ccc9dceb665b287e
kaikisk <kueobei@gmail.com>
2019-10-31 02:14:59 +0900

    commit

--- javaScript/result.js
+++ javaScript/result.js
@@ -1,29 +1,34 @@
-$(function() {
-    var query = decodeURI(location.search);
-    var queryStr = query.slice(1).split("&");
-    var queries = {};
-    console.log(typeof query);
-    console.log("queryStr" + typeof queryStr);
-    console.log("query" + query);
-    console.dir(queryStr);
+$(function () {
+    var queryStr = decodeURI(location.search);
+    var result = {};
 
-    if(query){
-        $('#txtDate').val("");
-        $('#txtdetail').val("test");
-        $('select[name="type"]').val();
-    }else{
+    if (queryStr) {
+        var queryArr = queryStr.slice(1).split("&");
+        console.dir(queryArr);
+
+        queryArr.forEach(query => {
+            var query1 = query.split("=");
+            result[query1[0]] = query1[1];
+        });
+        console.log("result: "+result);
+        console.dir(result);
+
+        $('#txtDate').val(result.date);
+        $('#txtdetail').val(result.detail);
+        $('select[name="type"]').val(result.val);
+    } else {
         return;
     }
 });
 
-function resultRegistration(){
+function resultRegistration() {
     var date = $('#txtDate').val();
     var detail = $('#txtdetail').val();
-    var val = ''; 
+    var val = '';
     var res = 'OK';
-    for (var i=1; i<=7; i++) {
-        if (document.getElementById("RblExamination"+i).checked) {
-            val = $('#RblExamination'+i).val();
+    for (var i = 1; i <= 7; i++) {
+        if (document.getElementById("RblExamination" + i).checked) {
+            val = $('#RblExamination' + i).val();
             break;
         }
     }
@@ -42,9 +47,9 @@ function resultRegistration(){
         var L = results.length;
         results[L] = client;
     } else {
-        var results = [client]; 
+        var results = [client];
     }
     resultsString = JSON.stringify(results);
-    localStorage.setItem('results',resultsString);
-    location.href='menu.html';
+    localStorage.setItem('results', resultsString);
+    location.href = 'menu.html';
 }
