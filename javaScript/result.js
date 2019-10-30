188cb24e58cafa3b2e91d3f85905a67bd751e5b6
kaikisk <kueobei@gmail.com>
2019-10-30 13:54:24 +0900

    commit

--- javaScript/result.js
+++ javaScript/result.js
@@ -1,6 +1,13 @@
 $(function() {
     var query = decodeURI(location.search.slice(1).split("&"));
+    var queries = {};
     console.dir(query);
+    query.forEach(query => {
+        var queryArr = query.split('=');
+        queries[queryArr[0]] = queryArr[1];
+    });
+
+    console.log(queries);
 
     if(query){
         $('#txtDate').val("");
