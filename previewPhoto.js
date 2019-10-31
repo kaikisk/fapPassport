27f21f59e3e8498192f1dd4e5c364355824061ab
kaikisk <kueobei@gmail.com>
2019-10-31 22:21:27 +0900

    commit

--- previewPhoto.js
+++ previewPhoto.js
@@ -1,21 +1,36 @@
 var db;
 var i = 1;
-var request = indexedDB.open('camera');
-request.onsuccess = function (event){
-    db = event.target.result;
-    var ts = db.transaction(["store1"], "readonly");
-    var store = ts.objectStore("store1");
-    var request = store.openCursor();
-    request.onsuccess = function (event) {
-      if(event.target.result == null) {
-        return;
-      }
-      var image = document.getElementById("image-" + i);
-      i++;
-      var cursor = event.target.result;
-      var data = cursor.value;
-      console.log("key："  + cursor.key +  "  value：" + data.mayvalue);
-      image.src = URL.createObjectURL(data.mayvalue);
-      cursor.continue();
+var request = indexedDB.open('fapPassport');
+request.onsuccess = function (event) {
+  db = event.target.result;
+  var ts = db.transaction(["photo"], "readonly");
+  var store = ts.objectStore("store1");
+  var request = store.openCursor();
+  request.onsuccess = function (event) {
+    if (event.target.result == null) {
+      return;
     }
+    var image = document.getElementById("image-" + i);
+    i++;
+    var cursor = event.target.result;
+    var data = cursor.value;
+    console.log("key：" + cursor.key + "  value：" + data.mayvalue);
+    image.src = URL.createObjectURL(data.mayvalue);
+    cursor.continue();
+  }
+}
+
+function getNowID() {
+  return new Promise((resolve, reject) => {
+    //????????????
+    var key = "nowId";
+    var db;
+    var request = indexedDB.open('fapPassport');
+    request.onsuccess = event => {
+      db = event.target.result;
+      var ts = db.transaction(["photo"], "readwrite");
+      var store = ts.objectStore("photo");
+      var requestName = store.get(key);
+    }
+  })
 }
