536983bcbe4cfdf33fe673f1c8a0f120c9cda1d3
kaikisk <kueobei@gmail.com>
2019-10-31 22:55:11 +0900

    commit

--- camera.js
+++ camera.js
@@ -2,29 +2,29 @@ var imageCapture;
 var tempImage;
 var video = document.getElementById("myVideo"); // 適当にvideoタグのオブジェクトを取得
 // var constrains = { video:{facingMode: { exact: "environment" }}, audio: false }; // 映像・音声を取得するかの設定
-var constrains = { video: true, audio: false  }; // 映像・音声を取得するかの設定
+var constrains = { video: true, audio: false }; // 映像・音声を取得するかの設定
 
 
-navigator.mediaDevices.enumerateDevices()
-.then(devices => {
-    var videoSelect = document.getElementById("videoSource");
-    console.log(devices);
-    console.dir(devices);
-    for (let i = 0; i !== devices.length; ++i) {
-        const deviceInfo = devices[i];
-        const option = document.createElement('option');
-        option.value = deviceInfo.deviceId;
-        if (deviceInfo.kind === 'videoinput') {
-            option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
-            videoSelect.appendChild(option);
-        }
-    }
-});
+// navigator.mediaDevices.enumerateDevices()
+// .then(devices => {
+//     var videoSelect = document.getElementById("videoSource");
+//     console.log(devices);
+//     console.dir(devices);
+//     for (let i = 0; i !== devices.length; ++i) {
+//         const deviceInfo = devices[i];
+//         const option = document.createElement('option');
+//         option.value = deviceInfo.deviceId;
+//         if (deviceInfo.kind === 'videoinput') {
+//             option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+//             videoSelect.appendChild(option);
+//         }
+//     }
+// });
 
 navigator.mediaDevices.getUserMedia(constrains)
-.then(gotStream).catch(function(err) {
-    console.log("An error occured! " + err);
-});
+    .then(gotStream).catch(function (err) {
+        console.log("An error occured! " + err);
+    });
 
 function gotStream(stream) {
     video.srcObject = stream; // streamはユーザーのカメラとマイクの情報で、これをvideoの入力ソースにする
@@ -56,30 +56,43 @@ function gotStream(stream) {
 // }
 
 function takePhoto() {
-    imageCapture.takePhoto().then(blob => {
-        console.log('Photo taken: ' + blob.type + ', ' + blob.size + 'B');
-        tempImage = blob;
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    //videoの縦幅横幅を取得
+    var w = video.offsetWidth;
+    var h = video.offsetHeight;
+    canvas.setAttribute("width", w);
+    canvas.setAttribute("height", h);
+    ctx.drawImage(video, 0, 0, w, h);
+    var temp = $(".video").html()
+    console.log("temp: " + temp);
+    console.log("url: " + URL.createObjectURL(blob));
+    $(".video").html(temp + '<img src="' + URL.createObjectURL(blob));
 
-        console.log(tempImage);
-        var temp = $(".video").html()
-        $(".video").html(temp + '<img src="' + URL.createObjectURL(blob));
-    })
-    .catch(err => console.error('takePhoto() failed: ', err));
+    // imageCapture.takePhoto().then(blob => {
+    //     console.log('Photo taken: ' + blob.type + ', ' + blob.size + 'B');
+    //     tempImage = blob;
+
+    //     console.log(tempImage);
+    //     var temp = $(".video").html()
+    //     $(".video").html(temp + '<img src="' + URL.createObjectURL(blob));
+    // })
+    // .catch(err => console.error('takePhoto() failed: ', err));
 }
 
-function save(){
+function save() {
     var db;
     var request = indexedDB.open("camera");
-    request.onsuccess = function(event){
+    request.onsuccess = function (event) {
         db = event.target.result;
         var keyName = document.getElementById("key").value; // キー名を取ってくる
         var ts = db.transaction(["store1"], "readwrite");
         var store = ts.objectStore("store1");
-        var request = store.put({mykey: keyName, mayvalue: tempImage});
-        request.onsuccess = function(){
+        var request = store.put({ mykey: keyName, mayvalue: tempImage });
+        request.onsuccess = function () {
             console.log("success put img");
         }
-        request.onerror = function(){
+        request.onerror = function () {
             console.log("error put img");
         }
     }
