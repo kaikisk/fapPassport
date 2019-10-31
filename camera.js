6bf2f9160ec01c36a6c2a21c1aa7402701133259
kaikisk <kueobei@gmail.com>
2019-11-01 00:22:58 +0900

    commit

--- camera.js
+++ camera.js
@@ -1,8 +1,8 @@
 var imageCapture;
 var tempImage;
 var video = document.getElementById("myVideo"); // 適当にvideoタグのオブジェクトを取得
-// var constrains = { video:{facingMode: { exact: "environment" }}, audio: false }; // 映像・音声を取得するかの設定
-var constrains = { video: true, audio: false }; // 映像・音声を取得するかの設定
+// var constrains = { video:{facingMode: { exact: "environment" }, width: "720", height: "1280"}, audio: false }; // 映像・音声を取得するかの設定
+var constrains = { video: tarue, width: "720", height: "1280", audio: false }; // 映像・音声を取得するかの設定
 
 
 // navigator.mediaDevices.enumerateDevices()
