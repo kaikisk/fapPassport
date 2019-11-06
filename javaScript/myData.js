f58082f84927d9977bd7f96287216149ca2375bd
kaikisk <kueobei@gmail.com>
2019-11-06 14:27:17 +0900

    commit

--- javaScript/myData.js
+++ javaScript/myData.js
@@ -3,14 +3,16 @@ $(function() {
     'medi1','medi2','medi3',
     'anam','anam2','anam3',
     'txtName'];
-    var loadArray = [];
+    // var loadArray = [];
     for(var i = 0; i < keys.length; i++) {
-        loadArray.push(load(keys[i]));
+        load(keys[i]).then(value => {
+            $("#" + keys[i]).text(value);
+        }).catch(err => console.error(err));
     }
 
-    Promise.all(loadArray).then(values => {
-        for(var i = 0; i < values.length; i++) {
-            $("#" + key[i]).text(values[i]);
-        }
-    }).catch(errs => console.error(errs));
+    // Promise.all(loadArray).then(values => {
+    //     for(var i = 0; i < values.length; i++) {
+    //         $("#" + key[i]).text(values[i]);
+    //     }
+    // }).catch(errs => console.error(errs));
 });
